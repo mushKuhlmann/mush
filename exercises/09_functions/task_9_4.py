03ebb915ed8218657a07b6be48cdd112ec9dc5b2
mushKuhlmann <korjansky@gmail.com>
2020-07-05 17:36:41 +0300

    05.07.20

--- exercises/09_functions/task_9_4.py
+++ exercises/09_functions/task_9_4.py
@@ -35,3 +35,25 @@ def ignore_command(command, ignore):
     * False - если нет
     '''
     return any(word in command for word in ignore)
+
+
+
+def convert_config_to_dict(config_filename):
+	f = open(config_filename)
+	dict_config = {}
+	last_word = ''
+	list_dict = []
+	list_config_filename = f.read().rstrip().split('\n')
+	list_config_filename.remove('')
+	for element in list_config_filename:
+		if not (element.startswith('!') or ignore_command(element,ignore) == True):
+			if not element.startswith(' '):
+				last_word = element
+				list_dict = []
+				dict_config.update({last_word:list_dict})
+			else:
+				element = element.strip()
+				list_dict.insert(1, element)
+				dict_config.update({last_word:list_dict})
+	return dict_config
+print(convert_config_to_dict('config_sw1.txt'))
