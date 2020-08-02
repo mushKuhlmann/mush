bb8855e7e0d964c0e5b36fc6cbdad6780a71a609
mushKuhlmann <korjansky@gmail.com>
2020-08-02 22:39:00 +0300

    02.08.20

--- exercises/12_useful_modules/task_12_3.py
+++ exercises/12_useful_modules/task_12_3.py
@@ -22,13 +22,12 @@ Reachable    Unreachable
 
 
 Для этого задания нет тестов
+1.создаем таблицу с заголовками 'Reachable', 'Unreachable' с помощью модуля  prettytable.
+2.тк по заданию нельзя изменять списки,которые приходят как аргументы, то использем функцию sorted, которая отсортирует входящие списки  и выдаст НОВЫЕ СПИСКИ, которые мы будем изменять.
+3. вычислим длину каждого списка. если один из списков короче , то добавим в конец списка пустой элемент '' до тех пор, пока длина обоих списков будет одинакова( с помощью цикла while).
+4.когда оба списка равны по длине, добавим элементы списка в таблицу построчно с помощью цикла for
 '''
-'''
-from tabulate import tabulate
-columns = ['Reachable', 'Unreachable']
-tabl = [('8.8.8.8','1.1.1.1'), ('9.9.9.9','')]
-print(tabulate(tabl, headers = columns))
-'''
+
 ip_list1 = ['10.1.1.1', '10.1.1.2', '10.1.1.10', '10.1.1.20']
 ip_list2 = ['1.1.1.1','10.1.1.7']
 from prettytable import PrettyTable
@@ -36,29 +35,19 @@ def print_ip_table(reachable, unreachable):
 	table = PrettyTable(['Reachable', 'Unreachable'])
 	k = 0
 	lenght = abs(len(unreachable) - len(reachable))
+	unreachable_use = sorted(unreachable)
+	reachable_use = sorted(reachable)
 	if len(reachable) < len(unreachable):
 		while k != lenght:
-			reachable.append('')
+			reachable_use.append('')
 			k = k + 1
 	elif len(reachable) > len(unreachable):
 		while k != lenght:
-			unreachable.append('')
+			unreachable_use.append('')
 			k = k + 1
-	else:
-		pass
-	for i in range(len(reachable)):
-		table.add_row([reachable[i],unreachable[i]])
-	if '' in reachable:
-		k = 0
-		while k != lenght:
-			reachable.remove('')
-			k = k + 1
-	elif '' in unreachable:
-		k = 0
-		while k != lenght:
-			unreachable.remove('')
-			k = k + 1
+	for i in range(len(reachable_use)):
+		table.add_row([reachable_use[i],unreachable_use[i]])
 	return table
-#print(print_ip_table(['10.1.1.1', '10.1.1.2'], ['10.1.1.7','10.1.1.8', '10.1.1.9']))
-print(print_ip_table(ip_list1, ip_list2))
-print(ip_list1, ip_list2)
+print(print_ip_table(['10.1.1.1', '10.1.1.2'], ['10.1.1.7','10.1.1.8', '10.1.1.9']))
+#print(print_ip_table(ip_list1, ip_list2))
+#print(ip_list1, ip_list2)
