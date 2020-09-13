6b278ea5e69f888450b65639e922ca1d0aee217b
mushKuhlmann <korjansky@gmail.com>
2020-09-13 23:12:11 +0300

    19.09.20

--- exercises/15_module_re/task_15_2.py
+++ exercises/15_module_re/task_15_2.py
@@ -21,3 +21,15 @@
 Проверить работу функции на примере файла sh_ip_int_br.txt.
 
 '''
+import re
+def parse_sh_ip_int_br(config_filename):
+	regex = r'(\S+)\s+(\d+.\d+.\d+.\d+|unassigned)\s+YES (manual|unset\s)\s(up|down|\S+ down)\s+(up|down)'
+	find_list = []
+	f = open(config_filename)
+	for line in f:
+		find = re.search(regex, line)
+		if find:
+			find_list.append(find.group(1 , 2 , 4, 5))
+
+	return find_list
+print(parse_sh_ip_int_br('sh_ip_int_br.txt'))
