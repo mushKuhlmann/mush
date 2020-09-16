d93eab1533d8e13ba4b9a94b88c5a2cdc2aac51e
mushKuhlmann <korjansky@gmail.com>
2020-09-16 19:31:37 +0300

    16.09.20

--- exercises/15_module_re/task_15_2.py
+++ exercises/15_module_re/task_15_2.py
@@ -19,15 +19,17 @@
 Для получения такого результата, используйте регулярные выражения.
 
 Проверить работу функции на примере файла sh_ip_int_br.txt.
+Результирующий список состоит из кортежей, которые являются НЕИЗМЕНЯЕМЫМИ, а значит нужно заменить administratively down на down ДО работы регулярного выражения. Мы сначала меняем в строке administratively down на down с помощью re.sub, а затем в уже ищем совпадение с регулярным выражением regex и добавляем результаты в список.
 
 '''
 import re
 def parse_sh_ip_int_br(config_filename):
-	regex = r'(\S+)\s+(\d+.\d+.\d+.\d+|unassigned)\s+YES (manual|unset\s)\s(up|down|\S+ down)\s+(up|down)'
+	regex = r'(\S+)\s+(\d+.\d+.\d+.\d+|unassigned)\s+YES (manual|unset\s)\s(up|down)\s+(up|down)'
 	find_list = []
 	f = open(config_filename)
 	for line in f:
-		find = re.search(regex, line)
+		new_line = re.sub('(administratively down)', 'down', line)
+		find = re.search(regex, new_line)
 		if find:
 			find_list.append(find.group(1 , 2 , 4, 5))
 
